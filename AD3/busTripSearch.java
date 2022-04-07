209eee96c569dca30cef273ce42526eff3bb3e7a
micahdarcy <micahdarcy@localhost>
2022-04-07 09:50:26 +0100

    Set-up busTripSearch

--- AD3/busTripSearch.java
+++ AD3/busTripSearch.java
@@ -1,4 +1,20 @@
+import java.io.IOException;
 
+/** @author micahdarcy
+ * 
+ * searching for all trips with a given arrival time 
+ * 
+ *
+ */
 public class busTripSearch {
+	
+	public static void deleteTrip() {
+		//delete invalid times 
+		
+	}
+
+	public static void main (String[] args) throws IOException{
+	// call constructor 
+	}
 
 }
